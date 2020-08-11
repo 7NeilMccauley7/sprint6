4af832f075006dd2443bc3b304afcf0413b6e860
unknown <7NeilMccauley7>
2020-08-11 23:10:37 +0200

    már sprint 9

--- js/main.js
+++ js/main.js
@@ -3,35 +3,81 @@ console.log("Hello this is a console massage");
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
-
-
-    /*isNan parancs beépített fügvény. Adunk neki egy változót és megmondja róla hogy Nan vagy nem NaN*/
-    /*
-    if (isNaN(amountNumber)) {
-        amountNumber = 0;
-    }
-
-*/
-        /*3 operandusos kifejezés. azt mondja ki az isNaN(amountNumber)-ban hogy igaz e az hogy isNaN az amountNumber.
-        HA igaz akkor a ? után értéket fogja visszadni. Ha NEM igaz akkor a : pont utánit. */ 
-
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
-  
     
-    if (amountNumber > 10) {
-        alert("Maximum 10 terméket vásárolhat!");
-       
-    } else if (amountNumber < 1) {
-        alert("Minimum 1 terméket kell vásárolnia!");
-    } else {
-
-    let amount = amountNumber * price;
-    showAmount.innerHTML = amount;
-    }
-
+    /*itt paraméterben adom meg(paraméterként) a showSumPrice 2 function két argumentumát */
     
-    /*alert("Fizetendő: " + amount + "Ft");*/ /*- alert*/
+    showSumPrice(price, amountNumber);
+}
+function showSumPrice(price, amountNumber) {
+    let showAmount = document.querySelector("span.show-amount");
+    
+        if (amountNumber > 10) {
+            alert("Maximum 10 terméket vásárolhat!");
+           
+        } else if (amountNumber < 1) {
+            alert("Minimum 1 terméket kell vásárolnia!");
+        } else {
+    
+        let amount = amountNumber * price;
+        showAmount.innerHTML = amount;
+     
+    }   
 }
 
+
+/*event hogy mi történtjen ha az elemre kattintok*/ 
+
+let sendButton = document.querySelector("#orderForm > button");
+
+/*event az addEventLisener-rel. Ezzel több eseményt lehet megadni*/
+/*sendButton.addEventListener("click", function() {
+    alert("Hello JS!");
+
+});
+*/
+
+
+//Űrlap
+let orderForm = document.querySelector("#orderForm");
+orderForm.addEventListener("Submit", function(ev){
+    /*ez azt csinálja hogyha rákattintanak a gombra akkor le fog frissülni az oldal*/
+  
+    ev.preventDefault();
+    console.log( this );
+
+});
+
+
+
+
+
+
+
+
+/* lekéri az ablak méretét a console log-ba
+window.addEventListener("resize", function() {
+        console.log(this.innerHeight, this.innerWidth);
+
+    })
+*/
+
+/*
+sendButton.onclick = function () {
+        alert("Hello JS!");
+}*/
+
+/*
+
+//Add helptext - hozzáad egy új elemet a html-nek
+let helpText = document.createElement("small");
+helpText.className = "form-text text-muted";
+helpText.innerHTML = "Adja meg a feltéteket!";
+
+let parent = document.querySelector("div.form-group:nth-child(1)");
+parent.appendChild(helpText);
+*/
+/*
+//elvesz szülőelemből egy elemet a html-nek
+parent.removeChild(helpText);*/
